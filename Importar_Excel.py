48c36f1593ef3384a6d969f0e6902b4af2897739
SAUDITH DAVID BECERRA <nestorandres4@hotmail.com>
2025-01-05 11:29:14 -0500

    Boton de filas
     Se puso el boton de filas pero no hace nada

---
+++ Importar_Excel.py
@@ -0,0 +1,55 @@
+from PyQt6.QtWidgets import QMainWindow, QApplication, QMessageBox, QTableWidgetItem
+from QT_Designer.Seleccionar_Hoja import Ui_Importar_Archivo
+from tkinter import filedialog
+import pandas as pd
+from tkinter import *
+from Mostrar_Excel import Tables
+
+
+
+class Import(QMainWindow):
+    def __init__(self):
+        super(Import, self).__init__()
+        self.ui = Ui_Importar_Archivo()
+        self.ui.setupUi(self)
+
+        self.ui.Boton_insert.clicked.connect(self.importar)
+        self.ui.Boton_conti.clicked.connect(self.continuar)
+
+        self.archivo_seleccionado = ""
+        self.hoja_seleccionada = ""
+
+    def importar(self):
+        archivo = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx;*.xls")])
+        if archivo:
+            try:
+                excel_file = pd.ExcelFile(archivo)
+                hojas = excel_file.sheet_names
+                self.ui.Caja_Archivo.clear()
+                self.ui.Caja_Archivo.addItems(hojas)
+                self.ui.lineEdit.setText(archivo)
+                self.archivo_seleccionado = archivo  
+            except Exception as e:
+                QMessageBox.critical(self, "Error", f"No se pudo leer el archivo.\nDetalles: {e}")
+
+    def continuar(self):
+        self.hoja_seleccionada = self.ui.Caja_Archivo.currentText()
+        if self.hoja_seleccionada == "Seleccione Hoja" or not self.hoja_seleccionada:
+            QMessageBox.warning(self, "Advertencia", "Por favor, seleccione una hoja.")
+        else:
+            QMessageBox.information(self, "Hoja Seleccionada", f"Has seleccionado la hoja: {self.hoja_seleccionada}")
+            self.abrir_tabla()
+
+    def abrir_tabla(self):
+        self.tabla = Tables(self.archivo_seleccionado, self.hoja_seleccionada)
+        self.tabla.mainloop()
+
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QApplication(sys.argv)
+    ventana = Import()
+    ventana.show()
+    sys.exit(app.exec())
---
+++ Mostrar_Excel.py
@@ -0,0 +1,51 @@
+from tkinter import *
+import pandas as pd
+from pandastable import Table
+from PIL import Image, ImageTk
+
+class Tables(Tk):
+    def __init__(self, archivo, hoja, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.archivo = archivo
+        self.hoja = hoja
+
+        self.frame = Frame(self)
+        self.frame.pack(fill=BOTH, expand=1)
+        self.table = Table(self.frame, dataframe=self.lectura(), showtoolbar=False, showstatusbar=True, editable=True)
+        self.table.show()
+
+        self.tool_frame = Frame(self)
+        self.tool_frame.pack(fill=X)
+
+        self.ifila = Image.open(r"Iconos/fila.png")
+        self.ifila = self.ifila.resize((30, 30), Image.Resampling.LANCZOS)
+        self.ifila = ImageTk.PhotoImage(self.ifila)
+        self.key = Button(self.tool_frame, command=self.modificar, image=self.ifila, compound="left")
+        self.key.pack(side=LEFT, padx=10, pady=10)
+
+        self.key.bind("<Enter>", self.mostrar_tooltip)
+        self.key.bind("<Leave>", self.ocultar_tooltip)
+
+        self.tooltip = Label(text="Modificar los indices", bg="yellow", fg="black")
+        self.tooltip.pack_forget()
+
+    def lectura(self):
+        try:
+            data = pd.read_excel(self.archivo, sheet_name=self.hoja)
+            return data
+        except Exception as e:
+            print(f"Error al leer el archivo: {e}")
+            return pd.DataFrame()
+
+    def modificar(self):
+        pass
+
+    def mostrar_tooltip(self, event):
+        self.tooltip.place(x=event.x_root, y=event.y_root -30)  
+
+    def ocultar_tooltip(self, event):
+        self.tooltip.place_forget()
+
+if __name__ == "__main__":
+    app = Tables("ruta/a/tu/archivo.xlsx", "NombreHoja")
+    app.mainloop()
